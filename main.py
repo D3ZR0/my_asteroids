6da3152ec260ee9705afe442144db5f8a85fe2ba
D3ZR0 <dwilcox0526@hotmail.com>
2025-06-30 23:54:03 -0400

    C: I finally got the screen working!

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from constants import *
+import pygame
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Hello from my-asteroids!")
-
-
-if __name__ == "__main__":
-    main()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    while True:
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
 
