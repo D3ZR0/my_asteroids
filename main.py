0450b39cbe6334dcd54d28bfd776ad9d73410a5d
D3ZR0 <dwilcox0526@hotmail.com>
2025-06-30 23:14:04 -0400

    B: fixed issues and preparing to push

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from constants import *
+
+
 def main():
     print("Hello from my-asteroids!")
 
 
 if __name__ == "__main__":
     main()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+
